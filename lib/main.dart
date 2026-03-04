688ac6981afd4e73e9835eeaf79064a2e9dbb62c
Hans-Vernier <36340605+hverby@users.noreply.github.com>
2026-03-04 15:21:32 +0000

    chore: wire up DI, fix lint issues, remove unused intl dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:wallet_app/features/home/presentation/screens/home_screen.dart';
+import 'core/theme/app_theme.dart';
+import 'features/home/presentation/screens/home_screen.dart';
+import 'injection_container.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const MyApp());
 }
 
@@ -12,11 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        textTheme: GoogleFonts.spaceGroteskTextTheme(),
-      ),
+      title: 'Wallet App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
       home: const HomeScreen(),
     );
   }
