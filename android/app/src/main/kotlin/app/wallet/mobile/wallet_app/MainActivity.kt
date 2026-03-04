f6c132fc869f4fbbf16c34663c676478c84d0f54
Hans-Vernier <36340605+hverby@users.noreply.github.com>
2026-03-04 12:18:56 +0000

    init

---
+++ android/app/src/main/kotlin/app/wallet/mobile/wallet_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package app.wallet.mobile.wallet_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:wallet_app/features/home/presentation/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: GoogleFonts.spaceGroteskTextTheme(),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
